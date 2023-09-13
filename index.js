786983050dc9512782f68cbe51fb3c0b97c4bb01
Umar2468-tech <areebfoodcorner@gmail.com>
2023-09-14 00:24:56 +0500

    Percentage Calculator Using Function

---
+++ index.js
@@ -0,0 +1,14 @@
+function Muhammad(){
+   let std_Total_Marks = prompt("Enter your total marks");
+let std_Obtained_Marks = prompt("Enter Your Otained Marks");
+ let res = (std_Obtained_Marks / std_Total_Marks) * 100;
+alert("You Have gotted " + res + "%")
+document.write( "You Have gotted " + res + "%")
+} 
+Muhammad();
+Muhammad();
+Muhammad();
+Muhammad();
+Muhammad();
+Muhammad();
+Muhammad();
